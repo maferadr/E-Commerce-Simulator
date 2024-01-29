65fb1994c4ac628ba49d84ed94a0ec8ac42aaa8b
Mafer Angulo <maferadr@Mafers-Air.lan>
2024-01-29 14:11:39 -0500

    Models file done

--- models/Product.js
+++ models/Product.js
@@ -8,7 +8,35 @@ class Product extends Model {}
 
 Product.init(
     {
-        //define columns
+        //columns defined
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        product_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        price: {
+            type: DataTypes.DECIMAL,
+            allowNull: false,
+            //Validation for decimal value missing
+        },
+        stock: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        category_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'category',
+                key: 'id',
+            }
+        }
     },
     {
         sequelize,
